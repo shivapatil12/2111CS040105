465a436294396952dc081184225cd03cc6e48c2f
Shiva Patil <118912633+shivapatil12@users.noreply.github.com>
2024-08-17 12:05:20 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React from 'react';
 import './App.css';
+import './App.test'
+import AverageCalculator from './AverageCalculator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Average Calculator Microservice</h1>
+                <AverageCalculator />
+            </header>
+        </div>
+    );
 }
 
 export default App;
